f29ca0a7fdc15cd6f0560b88158b7416850aecc1
David G <david.a.grs@gmail.com>
2018-01-19 00:05:01 +0100

    adding typemap skel

---
+++ typemap.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+template <typename T, typename... Args>
+struct TypeMap
+{
+	using TupleT = std::tuple<Args...>;
+
+	template <typename K>
+	const T& Get() const;
+
+	template <typename K>
+	T& Get();
+
+private:
+	std::vector<T> mElements;
+};
+
+template <typename T, typename... Args>
+template <typename K>
+const T& TypeMap<T, Args...>::Get() const
+{
+	return mElements[0];
+}
+
+template <typename T, typename... Args>
+template <typename K>
+T& TypeMap<T, Args...>::Get()
+{
+	return mElements[0];
+}
+
